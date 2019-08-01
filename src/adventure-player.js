3541fd55b327c1f432560d5f6ca2263cc8fe1d67
Gamaiel Zavala <gamaiel@yahoo.com>
2019-08-01 12:59:39 -0700

    Call setObjective in correct order

--- src/adventure-player.js
+++ src/adventure-player.js
@@ -6,13 +6,13 @@
 var util = SimpleUtil,
     WALK_DIST  = 1,
     WALK_TIME  = 25,
-    
+
     CHAR_START = 2,
     CHAR_END   = 6,
     CHAR_LR    = 1,
     CHAR_DOWN  = 2,
     CHAR_UP    = 3,
-    
+
     // dir values based on arrow keycodes
     DIR_N = 38,
     DIR_S = 40,
@@ -24,11 +24,11 @@ AdventurePlayer = function (path, opts, cb)
 {
     var self = this,
         keyFunc = function(e){ self.animateWalk(e); };
-    
+
     opts ? null : opts = {};
 
     AdventurePlayer.parent.constructor.apply(self, arguments);
-    
+
     util.setStyles(self.container, {
         transitionProperty: 'top, left',
         transitionTimingFunction: 'linear'
@@ -37,15 +37,15 @@ AdventurePlayer = function (path, opts, cb)
 
     // must be called after self.container is set
     self.setDuration();
-    
+
     //util.listen(window, 'keydown', keyFunc);
     //util.listen(window, 'keyup', keyFunc);
-    
+
     util.listen(self.container, util.resolvePrefix('transitionend'), function(e)
     {
         var prop = e.propertyName,
             value;
-        
+
         switch(prop)
         {
             case 'left':
@@ -53,7 +53,7 @@ AdventurePlayer = function (path, opts, cb)
                 self.walkPath();
         }
     });
-    
+
     //setInterval(function(){ self.walk(); }, WALK_TIME);
 }
 
@@ -66,19 +66,19 @@ util.extend(AdventurePlayer, AdventureItem,
     lastZ: 0,
     objective: null,
     movePayload: null,
-    
+
     getPublicMethods: function()
     {
         return ['cycleDialog', 'collidesWith', 'get', 'getBounds', 'getCoords', 'getHeight', 'getWidth', 'getXY',
                 'has', 'isAt', 'on', 'say', 'setObjective', 'setXY', 'walkTo'];
     },
-    
+
     animateWalk: function(e)
     {
         var self = this,
             dir = e.dir || e.keyCode,
             handled;
-        
+
         if(dir === DIR_IDLE || e.type === 'keyup')
         {
             self.setFrame(self._sprite.cols*(self.prevFrame-1)+1);
@@ -90,7 +90,7 @@ util.extend(AdventurePlayer, AdventureItem,
             if (self.lastDir == DIR_W || self.lastDir === null) {
                 self.mirror(false);
             }
-            
+
             switch(dir)
             {
                 case DIR_N:
@@ -115,26 +115,26 @@ util.extend(AdventurePlayer, AdventureItem,
                     handled = true;
                 break;
             }
-            
+
             self.play();
-            
+
             self.lastDir = handled ? dir : null;
         }
-        
+
         if(self.lastDir && e.keyCode){
             util.processEvent(e, true);
         }
     },
-    
+
     walkPath: function()
     {
         var self = this,
             path = self.path;
-        
+
         if (self.movePayload) {
             self.fire('aftermove', self.movePayload);
         }
-        
+
         if(path.length)
         {
             var node = path.shift(),
@@ -150,7 +150,7 @@ util.extend(AdventurePlayer, AdventureItem,
                 isDiagonal = curX !== x && curY !== y,
                 duration = isDiagonal ? 2.0 : 1.0,
                 dir = self.coordsToDir([x, y]);
-            
+
             //self.tileAt([x, y], true); // debug
             self.movePayload = {from: [curX, curY], to: [x, y], done: path.length === 1};
             self.setDuration(duration);
@@ -161,64 +161,65 @@ util.extend(AdventurePlayer, AdventureItem,
         else
         {
             var target = self.target;
-            
+
             self.animateWalk({dir: DIR_IDLE});
-            
+
             // face the target if present
             if (target) {
                 self.face(target);
                 self.target = null;
             }
-            
+
             self.completeObjective();
         }
     },
-    
+
     walkTo: function(coords, objective)
     {
         var self = this,
             path;
-        
+
         if (coords instanceof AdventureItem) {
             self.target = coords;
             coords = self.target.getWalkTo();
         } else if (objective) {
             self.target = objective.target;
         }
-        
+
         path = self.coordsToPath(coords);
         if (path && path.length) {
             self.setPath(path);
+            self.setObjective(objective);
         // TODO verify player walked to target?
         } else {
+            self.setObjective(objective);
             self.completeObjective();
         }
-        
-        self.setObjective(objective);
+
     },
-    
+
     completeObjective: function()
     {
         var self = this,
             objective = self.objective || {};
-        
+
         if (util.isFunc(objective)) {
             objective();
         } else if (objective.target) {
             objective.target.fire(objective.action);
         }
-        
+
         // clear objective
         self.setObjective(null);
     },
-    
+
     face: function(obj)
     {
         var self = this;
         self.animateWalk({dir: self.coordsToDir(obj)});
         self.animateWalk({dir: DIR_IDLE});
     },
-    
+
     coordsToDir: function(obj)
     {
         var self = this, x, y,
@@ -227,17 +228,17 @@ util.extend(AdventurePlayer, AdventureItem,
             collides = true,
             coords,
             dir;
-        
+
         if (obj instanceof AdventureItem) {
             coords = [obj.getX(), obj.getY()];
             collides = obj.collidesWith(self.getBounds());
         } else {
             coords = obj;
         }
-        
+
         x = coords[0];
         y = coords[1];
-        
+
         switch(true)
         {
             case curY > y && collides:
@@ -249,10 +250,10 @@ util.extend(AdventurePlayer, AdventureItem,
             case curY < y:
                 dir = DIR_S; break;
         }
-        
+
         return dir;
     },
-    
+
     setPath: function(path)
     {
         var self = this;
@@ -260,19 +261,19 @@ util.extend(AdventurePlayer, AdventureItem,
         self.setObjective(null);
         self.walkPath();
     },
-    
+
     setObjective: function(objective)
     {
         var self = this;
         self.objective = objective;
     },
-    
+
     isAt: function(asset)
     {
         if (!(asset instanceof AdventureAsset)) {
             //return false;
         }
-        
+
         var self = this,
             walkTo = asset.getWalkTo(),
             player = asset.getPlayer(),
@@ -288,27 +289,27 @@ util.extend(AdventurePlayer, AdventureItem,
 
         return false;
     },
-    
+
     setDuration: function(multiplier)
     {
         var self = this,
             duration = self.walkSpeed / 1000 * (multiplier||1) + 's';
-        
+
         util.setStyle(self.container, 'transitionDuration', duration, true);
-        
+
         return duration;
     },
-    
+
     getCoords: function()
     {
         var self = this,
             origin = self.getOrigin(),
             x = self.getX() + origin[0],
             y = self.getY() + origin[1];
-        
+
         return [x, y];
     },
-    
+
     getOrigin: function()
     {
         var self = this,
@@ -316,37 +317,37 @@ util.extend(AdventurePlayer, AdventureItem,
             h = self.height,
             x = Math.round(w/2),
             y = h;
-        
+
         return [x, y];
     },
-    
+
     isWalkable: function(x, y, dir)
     {
         var self = this,
             h = self.height,
             w = self.width;
-        
+
         return self.tileAt([x, y + h])[2];
     },
-    
+
     isWalking: function()
     {
         return this.isPlaying();
     },
-    
+
     setXY: function(x, y, rel)
     {
         var self = this;
         AdventurePlayer.parent.setXY.apply(self, arguments);
         self.setZ();
     },
-    
+
     setZ: function()
     {
         var self = this;
         util.setStyle(self.container, 'zIndex', self.getMaxZ());
     },
-    
+
     serialize: function()
     {
         var self = this;
@@ -356,7 +357,7 @@ util.extend(AdventurePlayer, AdventureItem,
         self.attrs.x = parseInt(self.getStyle('left'), 10);
         self.attrs.y = parseInt(self.getStyle('top'), 10);
     },
-    
+
     /*
     walk: function()
     {
@@ -365,9 +366,9 @@ util.extend(AdventurePlayer, AdventureItem,
             sprite = self,
             x = self.getX(),
             y = self.getY();
-        
+
         if(!dir) return;
-        
+
         switch(dir)
         {
             case DIR_N:
@@ -393,21 +394,21 @@ util.extend(AdventurePlayer, AdventureItem,
         }
     },
     */
-    
+
     // the following methods are augmented by Adventure
-    
+
     say: function(msg)
     {
     },
-    
+
     tileAt: function()
     {
     },
-    
+
     getTileSize: function()
     {
     },
-    
+
     getMaxZ: function()
     {
     }
