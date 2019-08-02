09975554f2898575bea149d5fa1d4de2fb97d674
Gamaiel Zavala <gamaiel@yahoo.com>
2019-08-01 17:19:49 -0700

    Add petaled frame

--- game/items/chickenleg.js
+++ game/items/chickenleg.js
@@ -5,13 +5,13 @@ var util = SimpleUtil;
 GameChickenLeg = function(path, opts, cb)
 {
     var self = this;
-    
+
     opts = util.merge(opts || {}, {
       width: 25, height: 12, coords: [214, 119], walkTo: [-30, 70], layerPoint: [0, 50], zIndex: 80,
       pocketable: true, title: "chicken leg",
       description: "A juicy chicken leg."
     });
-    
+
     // use call since opts has been modified
     GameChickenLeg.parent.constructor.apply(self, arguments);
 };
@@ -22,17 +22,18 @@ util.extend(GameChickenLeg, AdventureItem,
     {
         var chickenleg = this,
             player = chickenleg.getPlayer(),
+            inventory = chickenleg.getInventory(),
             target = e.target;
-        
+
         switch (target.name) {
             case "petals":
                 if (chickenleg.isNot("petaled")) {
                     player.say("I added some petals to the chicken leg. Mmm, herbed chicken.");
                     chickenleg.set("petaled");
+                    chickenleg.setFrame(3);
                     chickenleg.setTitle("herbed chicken leg");
                     chickenleg.setDesc("A juicy herbed chicken leg.");
-                } else {
-                    player.say("It's already herbed.");
+                    inventory.remove("petals");
                 }
             break;
             default: return true;
